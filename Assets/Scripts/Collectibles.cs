7a90281f2cd3766fd47dc010a9ca47b12413db1e
EngTreinamento <eng.pr.mkt.2@eng.com.br>
2026-08-18 11:05:46 -0300

    Feito lógica de coletáveis e documentação de código

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
+    //Referência da Data de cada coletável, onde podemos mudar qual vai ser o score e a aparência de cada coletável
     [SerializeField] CollectibleData collectible;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GetComponent<MeshRenderer>().material.color = collectible.color;
@@ -15,5 +15,41 @@ public class Collectibles : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //Métodos de colisão para verificar se o player encostou no coletável,
+    //para daí fazer as suas ações no OnPlayerCollision, tanto trigger quando colisão padrão
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Estamos verificando a colisão por tag, pois o player não vai fazer nada quando isso colidir,
+        //apenas o coletável
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Através de Colisão normal");
+            OnPlayerCollision();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Estamos verificando a colisão por tag, pois o player não vai fazer nada quando isso colidir,
+        //apenas o coletável
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Através de Colisão trigger");
+            OnPlayerCollision();
+        }
+    }
+
+    private void OnPlayerCollision()
+    {
+        // Primeiro ele vai procurar o ScoreManager na cena
+        ScoreManager scoreManager = FindObjectOfType<ScoreManager>();
+
+        // Depois ele vai usar o método do ScoreManager para adicionar a pontuação
+        scoreManager.AddScore(collectible.score);
+
+        // Por fim, ele vai destruir o coletável para limpar a cena
+        Destroy(gameObject);
     }
 }
